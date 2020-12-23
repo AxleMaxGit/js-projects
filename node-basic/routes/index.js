48abeace05b55e1edf56659d6e728b7265ddd46a
AxleMaxGit <AxleMaxGit@users.noreply.github.com>
2020-12-24 07:08:36 +1000

    move routes to routes/index.js

---
+++ node-basic/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const people = require('../people.json');
+
+// Render views
+router.get('/', (req, res) => {
+    // res.send(HTML);
+    res.render('index', {
+        title: 'Homepage',
+        people: people.profiles
+    })
+  });
+
+  router.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
+  });
+
+  module.exports = router;
